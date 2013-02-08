a4b4b385bcec31f57bc3518bc150c03054f64874
Ragnis Armus <ragnis.armus@gmail.com>
2013-02-08 22:31:45 +0200

    Setup Travis CI

---
+++ util/travis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+root=$(git rev-parse --show-toplevel)
+
+ruby "$root/util/validator/validate.rb"
+
+if [ "$?" -eq 0 ]; then
+	# Tell our web server to try to deploy the data
+	curl -X POST --silent "http://deploy.axr.vg/website-data.php" > /dev/null
+else
+	exit 1
+fi
+
+echo "Finished"
