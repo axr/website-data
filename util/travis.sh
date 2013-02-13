55450329d938c869b33d80c64bae57e22a396866
Ragnis Armus <ragnis.armus@gmail.com>
2013-02-13 14:37:04 +0200

    Use the new domain for deploying

--- util/travis.sh
+++ util/travis.sh
@@ -7,7 +7,7 @@ ruby "$root/util/validator/validate.rb"
 
 if [ "$?" -eq 0 ]; then
 	# Tell our web server to try to deploy the data
-	curl -X POST --silent "http://deploy.axr.vg/website-data.php" > /dev/null
+	curl -X POST --silent "http://deploy.axrproject.org/website-data.php" > /dev/null
 else
 	exit 1
 fi
